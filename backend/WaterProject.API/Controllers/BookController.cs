e86b87a330756b4dbc9d96455931b5dab9bbbf54
daynes13 <153124640+daynes13@users.noreply.github.com>
2025-03-28 19:45:22 -0600

    Phase 4 final

--- backend/WaterProject.API/Controllers/BookController.cs
+++ backend/WaterProject.API/Controllers/BookController.cs
@@ -15,10 +15,25 @@ namespace Bookstore.API.Controllers
             _bookContext = temp;
         }
 
-        [HttpGet]
-        public IActionResult Get(int pageSize = 5, int pageNum = 1, bool sortByTitle = false)
+        [HttpGet("AllBooks")]
+        public IActionResult Get(int pageSize = 5, int pageNum = 1, bool sortByTitle = false, [FromQuery] List<string>? bookTypes = null)
         {
             var query = _bookContext.Books.AsQueryable();
+
+            if (bookTypes != null && bookTypes.Any())
+            {
+                query = query.Where(b => bookTypes.Contains(b.Category));
+            }
+
+            HttpContext.Response.Cookies.Append("FavoriteBook", "Les Miserables", new CookieOptions
+            {
+                HttpOnly=true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.Now.AddMinutes(1),
+            });
+
+            
 
             // Apply sorting if requested
             if (sortByTitle)
@@ -40,6 +55,17 @@ namespace Bookstore.API.Controllers
             };
 
             return Ok(response);
+        }
+
+        [HttpGet("GetBookTypes")]
+        public IActionResult GetBookTypes()
+        {
+            var bookTypes = _bookContext.Books
+                .Select(b => b.Category)
+                .Distinct()
+                .ToList();
+
+            return Ok(bookTypes);
         }
     }
 }
--- backend/WaterProject.API/Program.cs
+++ backend/WaterProject.API/Program.cs
@@ -13,7 +13,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<BookstoreContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("BookstoreConnection")));
 
-builder.Services.AddCors();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name:"AllowFrontend",
+        configurePolicy: policy =>
+        {
+            policy.WithOrigins("http://localhost:3000").AllowCredentials().AllowAnyHeader().AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -24,7 +31,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors(x => x.WithOrigins("http://localhost:3000"));
+app.UseCors("AllowFrontend");
 
 app.UseHttpsRedirection();
 
